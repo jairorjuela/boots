9c8562ec5b26007593b2fd74b5262f8c81a80e9d
jairorjuela <genius85862@gmail.com>
2018-12-12 09:27:05 -0500

    Add product to 'Portafolio'

--- js/app.js
+++ js/app.js
@@ -494,6 +494,25 @@ $(document).ready(function () {
   }
   // Fin Modal 25
 
+  // Inicio Modal 26
+  var modal26 = document.getElementById('myModal26');
+
+  var img26 = document.getElementById('myImg26');
+  var modalImg26 = document.getElementById("img27");
+  var captionText26 = document.getElementById("caption26");
+  img26.onclick = function(){
+      modal26.style.display = "block";
+      modalImg26.src = this.src;
+      captionText26.innerHTML = this.alt;
+  }
+
+  var span26 = document.getElementById('close26');
+
+  span26.onclick = function() {
+      modal26.style.display = "none";
+  }
+  // Fin Modal 26
+
 
 
 
